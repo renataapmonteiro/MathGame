a5aa20d62d2f6f129649a9f39335200806d4ac3c
renataapmonteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-06-17 17:29:31 -0300

    logic implemented

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MathValidators } from '../math-validators'
+import { delay, filter, scan } from 'rxjs/operators'
 
 @Component({
   selector: 'app-equation',
@@ -8,12 +9,13 @@ import { MathValidators } from '../math-validators'
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
     answer: new FormControl('')
   }, [
-    MathValidators.addition
+    MathValidators.addition('answer', 'a', 'b')
   ]);
 
   constructor() { }
@@ -26,7 +28,33 @@ export class EquationComponent implements OnInit {
     return this.mathForm.value.b;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(){
+    // const startTime = new Date();
+    // let numberSolved = 0;
+
+    this.mathForm.statusChanges.pipe(
+      filter( value => value === 'VALID'),
+      delay(500),
+      scan((acc, value) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, { numberSolved:0, startTime: new Date()})
+    ).subscribe(({ numberSolved, startTime}) => {
+      // numberSolved++;
+
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000;
+
+      this.mathForm.setValue({
+        a:this.randomNumber(),
+        b:this.randomNumber(),
+        answer: ''
+      });
+    });
+  }
 
   randomNumber(){
     return Math.floor(Math.random()*10);
